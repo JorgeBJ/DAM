2c304d6fbd8e2b1e8a487c736c06febff605d04b
Elias Perianez <eliasperianez@gmail.com>
2019-09-23 13:09:46 +0200

    Conexion changed, added create killer build

--- conexion.php
+++ conexion.php
@@ -1,17 +1,24 @@
 
 <?php
 //Acceso a la Base de Datos
-$serverName="localhost";
-$userName="root";
-$pw="";
-$db="dbd_meta";
+
 
 //Creacion de la conexion
-$conn= new mysqli($serverName,$userName,$pw,$db);
+function create_con{
+    $serverName="localhost";
+    $userName="root";
+    $pw="";
+    $db="dbd_meta";
+    $conn = new mysqli($serverName,$userName,$pw,$db);
+    //Comprobando la conexión
+    if ($conn->connect_error) {
+        die("Conexion fallida: " . $conn->connect_error);
+    } 
+    return $conn;
+}
 
-//Comprobando la conexión
-if ($conn->connect_error) {
-    die("Conexion fallida: " . $conn->connect_error);
-} 
+function kill_con(mysqli $conn){
+  mysqli_close($conn);
+}
 
 ?>
---
+++ create_killer_build.php
@@ -0,0 +1,58 @@
+<?php
+require("conexion.php");
+//id`, `name`, `killerId`, `perk1Id`, `perk2Id`, `perk3Id`, `perk4Id`, `addon1Id`, `addon2Id`
+    if isset( $_POST['name'] && $_POST['killerId']){
+        $conn = create_con();
+        $name = $_POST['name'];
+        $perk1 = $_POST['perk1'];
+        $perk2 = $_POST['perk2'];
+        $perk3 = $_POST['perk3'];
+        $perk4 = $_POST['perk4'];
+        $addon1 = $_POST['addon1'];
+        $addon2 = $_POST['addon2'];
+        $fp = checkFp($killerId, $name, $perk1, $perk2, $perk3, $perk4, $conn);
+        $noob = checkNoobie($killerId, $name, $perk1, $perk2, $perk3, $perk4, $conn);
+        //INSERT INTO `killer_build`(`id`, `name`, `killerId`, `perk1Id`, `perk2Id`, `perk3Id`, `perk4Id`, `addon1Id`, `addon2Id`, `isF2P`, `isNoob`) VALUES ([value-1],[value-2],[value-3],[value-4],[value-5],[value-6],[value-7],[value-8],[value-9],[value-10],[value-11])
+        $query = 'INSERT INTO `killer_build`(`name`, `killerId`, `perk1Id`, `perk2Id`, `perk3Id`, `perk4Id`, `addon1Id`, `addon2Id`, `isF2P`, `isNoob`) VALUES ('.$name.','.$perk1.','.$perk2.','.$perk3.','.$perk4.','.$addon1.','.$addon2.','.$fp.','.$noob.')';
+    }else{
+        die;
+    }
+
+    function checkFp(int $killerId, int $perk1, int $perk2, int $perk3, int $perk4, mysqli $conn){
+        $isFp = TRUE;
+        $allowedKillers = ["The Trapper", "The Wraith", "The Hillbilly", "The Nurse", "The Huntress"];
+        $killer_name = mysqli_query($conn, 'SELECT `name` FROM `killers` WHERE `id` = '.$killerId)
+        if(!in_array($killer_name, $allowedKillers, FALSE)){
+            $isFp = FALSE;
+        }
+        $perkArray = [$perk1, $perk2, $perk3, $perk4];
+        foreach($perkArray as $perk){
+            $theKillerId = mysqli_query($conn, 'SELECT `killerId` FROM `perks` WHERE `id` = '.$perk);
+            if($theKillerId == NULL){
+                $theKiller == NULL
+            }else{
+                $theKiller = mysqli_query($conn, 'SELECT `name` FROM `killers` WHERE `id` = '.$theKillerId);
+            }
+            if(!$theKillerId == NULL){
+                if(!in_array($theKiller, $allowedKillers, FALSE)){
+                    $isFp = FALSE;
+            }
+        }
+        }
+        return $isFP;
+    }
+
+    function checkNoobie(int $killerId, int $perk1, int $perk2, int $perk3, int $perk4, mysqli $conn){
+        $isNoob = TRUE
+        $perkArray = [$perk1, $perk2, $perk3, $perk4];
+        foreach($perkArray as $perk){
+            $theKillerId = mysqli_query($conn, 'SELECT `killerId` FROM `perks` WHERE `id` = '.$perk);
+            if($theKillerId != NULL){
+                if(!$theKillerId == $killerId){
+                 $isNoob = FALSE;
+                }
+            }
+        }
+        return $isNoob
+    }
+?>
