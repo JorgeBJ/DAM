01782464803022e2cb48103776d723d379640c76
Elias Perianez <eliasperianez@gmail.com>
2019-11-08 11:35:25 +0100

    Get builds actualizado, bd actualizada

---
+++ getKillersBuilds.php
@@ -0,0 +1,17 @@
+<?php
+require("conexion.php");
+$conn = create_con();
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        //Establecer filtros
+    }else{
+        //Recuperar todas
+        $query = 'SELECT * FROM `killer_build`';
+        $result = $conn->query($query);
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                //Imprimir
+                echo($row);
+            }
+        }
+    }
+?>
