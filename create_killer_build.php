00bfbd6bab531e0adc57bed53242fb28d9a219cb
Elias Perianez <eliasperianez@gmail.com>
2019-12-15 18:58:04 +0100

    Final 1.0 Version

--- create_killer_build.php
+++ create_killer_build.php
@@ -8,7 +8,6 @@
     include_once 'conexion.php';
  
     $conn = create_con();
- // if isset( $_POST['nombreBuildAsesino'] && $_POST['killer_id']){
         $name = $_POST['nombreBuildAsesino'];
         $killerId = $_POST['killerId'];
         $perk1 = $_POST['seleccionPerk1Name'];
@@ -18,63 +17,12 @@
         $addon1 = $_POST['seleccionAddon1Name'];
         $addon2 = $_POST['seleccionAddon2Name'];
         $createdBy= "null";
-    //    if(isset($_SESSION["id"]){
-    //        $createdBy= $_SESSION["id"];
-     //   }else{
-     //       $createdBy= NULL;
-      //  }       
-       // $fp = checkFp($killer_id, $nombre, $perk1, $perk2, $perk3, $perk4, $conn);
-       //$noob = checkNoobie($killer_id, $name, $perk1, $perk2, $perk3, $perk4, $conn);
        $fp=1;
        $noob=1;
     $query = "INSERT INTO killer_build (build_name, killer_id, perk1_id, perk2_id, perk3_id, perk4_id, addon1_id, addon2_id, isF2P, isNoob, created_by) VALUES ('".$name."',".$killerId.",".$perk1.",".$perk2.",".$perk3.",".$perk4.",".$addon1.",".$addon2.",".$fp.",".$noob.",".$createdBy.")";
-//           $query= "INSERT INTO usuarios(nombre, email, password) VALUES('".$nombre."', '".$email."','".$password."')";
- //   }else{
-  //      die;
-   // }
-
-    // function checkFp(int $killer_id, int $perk1, int $perk2, int $perk3, int $perk4, mysqli $conn){
-    //     $isFp = TRUE;
-    //     $allowedKillers = ["The Trapper", "The Wraith", "The Hillbilly", "The Nurse", "The Huntress"];
-    //     $killer_name = mysqli_query($conn, 'SELECT `name` FROM `killers` WHERE `id` = '.$killer_id)
-    //     if(!in_array($killer_name, $allowedKillers, FALSE)){
-    //         $isFp = FALSE;
-    //     }
-    //     $perkArray = [$perk1, $perk2, $perk3, $perk4];
-    //     foreach($perkArray as $perk){
-    //         $theKiller_id = mysqli_query($conn, 'SELECT `killer_id` FROM `perks` WHERE `id` = '.$perk);
-    //         if($theKiller_id == NULL){
-    //             $theKiller == NULL
-    //         }else{
-    //             $theKiller = mysqli_query($conn, 'SELECT `name` FROM `killers` WHERE `id` = '.$theKiller_id);
-    //         }
-    //         if(!$theKiller_id == NULL){
-    //             if(!in_array($theKiller, $allowedKillers, FALSE)){
-    //                 $isFp = FALSE;
-    //         }
-    //     }
-    //     }
-    //     return $isFP;
-    // }
-
-    // function checkNoobie(int $killer_id, int $perk1, int $perk2, int $perk3, int $perk4, mysqli $conn){
-    //     $isNoob = TRUE
-    //     $perkArray = [$perk1, $perk2, $perk3, $perk4];
-    //     foreach($perkArray as $perk){
-    //         $theKiller_id = mysqli_query($conn, 'SELECT `killer_id` FROM `perks` WHERE `id` = '.$perk);
-    //         if($theKiller_id != NULL){
-    //             if(!$theKiller_id == $killer_id){
-    //              $isNoob = FALSE;
-    //             }
-    //         }
-    //     }
-    //     return $isNoob
-    // }
-
-
     $resultado=$conn->query($query);
-
-     if ($resultado === TRUE) {
+    
+    if ($resultado === TRUE) {
         $mensaje="<h1>Build añadida</h1>";
     } else {
         $mensaje="<h1>No se ha podido añadir la build </h1><p>" . $conn->error."</p>";
--- create_survivor_build.php
+++ create_survivor_build.php
@@ -17,9 +17,7 @@
         $itemID = $_POST['itemID'];
         $addon1 = $_POST['seleccionObjetoAddon1Name'];
         $addon2 = $_POST['seleccionObjetoAddon2Name'];
-       // $fp = checkFp($survivorId, $name, $perk1, $perk2, $perk3, $perk4, $º);
         $fp=true;
-        //$noob = checkNoobie($survivorId, $name, $perk1, $perk2, $perk3, $perk4, $conn);
         $noob=true;
         $query = "INSERT INTO survivor_build(survivorBuildName, survivorId, perk1Id, perk2Id, perk3Id, perk4Id, itemId, addon1Id, addon2Id, isF2P, isNoob) VALUES ('".$name."','".$survivorId."','".$perk1."','".$perk2."','".$perk3."','".$perk4."','".$itemID."','".$addon1."','".$addon2."','".$fp."','".$noob."')";
 
@@ -31,44 +29,6 @@
            $mensaje="<h1>No se ha podido añadir la build </h1><p>" . $conn->error."</p>";
        }
        kill_con($conn);
-/* 
-       function checkFp(int $survivorId, int $perk1, int $perk2, int $perk3, int $perk4, mysqli $conn){
-        $isFp = TRUE;
-        $allowedSurvivors = ["Claudette Morel", "Jake Park", "Dwight Fairfield", "Meg Thomas", "Nea Karlsson", "David King"];
-        $survivor_name = mysqli_query($conn, 'SELECT `name` FROM `survivors` WHERE `id` = '.$survivorId)
-        if(!in_array($survivor_name, $allowedSurvivors, FALSE)){
-            $isFp = FALSE;
-        }
-        $perkArray = [$perk1, $perk2, $perk3, $perk4];
-        foreach($perkArray as $perk){
-            $thesurvivorId = mysqli_query($conn, 'SELECT `survivorId` FROM `perks` WHERE `id` = '.$perk);
-            if($thesurvivorId == NULL){
-                $theSurvivor == NULL
-            }else{
-                $theSurvivor = mysqli_query($conn, 'SELECT `name` FROM `survivors` WHERE `id` = '.$thesurvivorId);
-            }
-            if(!$thesurvivorId == NULL){
-                if(!in_array($theSurvivor, $allowedSurvivors, FALSE)){
-                    $isFp = FALSE;
-            }
-        }
-        }
-        return $isFP;
-    }
-
-    function checkNoobie(int $survivorId, int $perk1, int $perk2, int $perk3, int $perk4, mysqli $conn){
-        $isNoob = TRUE
-        $perkArray = [$perk1, $perk2, $perk3, $perk4];
-        foreach($perkArray as $perk){
-            $thesurvivorId = mysqli_query($conn, 'SELECT `survivor_id` FROM `perks` WHERE `id` = '.$perk);
-            if($thesurvivorId != NULL){
-                if(!$thesurvivorId == $survivorId){
-                 $isNoob = FALSE;
-                }
-            }
-        }
-        return $isNoob
-    }   */
 ?>
        
        <body>
